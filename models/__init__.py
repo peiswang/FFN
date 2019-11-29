b3d04721623dbcbf99e375fc2125815d133bcbbc
Peisong Wang <peisong.wang@nlpr.ia.ac.cn>
2019-11-29 20:49:05 +0800

    fix time

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
 from .alexnet_bn import *
+from .vgg import *
 from .scale import Scale
 from .quan import Quantization
--- sdd.py
+++ sdd.py
@@ -4,7 +4,7 @@ import numpy as np, math, operator
 import time
 import pickle
 
-def sdd(A, kmax=100, alphamin=0.001, lmax=1000, rhomin=0, yinit=2, max_epoch=20, savedir = None):
+def sdd(A, kmax=100, alphamin=0.001, lmax=1000, rhomin=0, max_epoch=20, savedir=None, prefix=None):
     #   SDD  Semidiscrete Decomposition.
     
     ### Check Input Arguments    
@@ -15,8 +15,7 @@ def sdd(A, kmax=100, alphamin=0.001, lmax=1000, rhomin=0, yinit=2, max_epoch=20,
     
     if 'rhomin' in locals():
         rhomin = rhomin**2
-    idx = 0             # only used for yinit = 1 (python is zero-based contrary to matlab)
-    
+
     # Initialization    
     [m,n] = A.shape         # size of A
     res = (np.linalg.norm(A,'fro'))**2
@@ -26,64 +25,30 @@ def sdd(A, kmax=100, alphamin=0.001, lmax=1000, rhomin=0, yinit=2, max_epoch=20,
     ysav = np.zeros((n,kmax))
     dsav = np.zeros((kmax,))
     
+    A_copy = np.copy(A)
     # Outer loop
+    start_time = time.time()
     for k in range(0,kmax):
-        print('sdd init ' + str(k+1) + ' of ' + str(kmax))
-        # Initialize y for inner loop
-        if yinit == 0:          # Threshold
-            s = np.zeros((m,))
-            iits = 0
-            while math.pow(np.linalg.norm(s),2) < (float(rho)/n):                
-                y = np.zeros((n,))                     
-                y[idx] = 1
-                s = np.dot(A,y)
-                if k>0:       # python is zero-based             
-                    s = s - (np.dot(xsav,(np.multiply(dsav,(np.dot(ysav.T,y))))))                    
-                    
-                idx = np.mod(idx, n) + 1
-                if idx == n:        # When idx reaches n it should be changed to zero (otherwise an index out of bounds error will occur)
-                    idx = 0
-                iits = iits + 1
-            
-        elif yinit == 1:        # Cycling Periodic Ones
-            y = np.zeros((n,))
-            index = np.mod(k-1,n)+1
-            if index < n:                 
-                y[index] = 1
-            else:
-                y[0] = 1   
-        elif yinit == 2:        # All Ones
-            y = np.ones((n,))
-        elif yinit == 3:        # Periodic Ones
-            y = np.zeros((n,))
-            ii = np.arange(0,n,100)
-            for i in ii: # python is zero-based
-                y[i] = 1 
-        else:
-            try:
-                pass
-            except ValueError:
-                print('Invalid choice for C.')
+        # A_copy = A - np.dot(xsav * dsav, ysav.T)  ## about 10s
+        # A_copy = A - np.dot(xsav[:,:k] * dsav[:k], ysav[:,:k].T)
+        # Initialize y by all ones 
+        y = np.ones((n,))
                 
         # Inner loop
         for l in range (0,lmax):
             # Fix y and Solve for x
-            s = np.dot(A,y)            
-            if k > 0:       # python is zero-based
-                s = s - np.dot(xsav, np.multiply(dsav, np.dot(ysav.T, y)))
+            s = np.dot(A_copy,y)            
             [x, xcnt, _] = sddsolve(s, m)
 
             # Fix x and Solve for y
-            s = np.dot(x, A)
-            if k > 0:
-                s = s - np.dot(ysav, np.multiply(dsav, np.dot(xsav.T, x)))
+            s = np.dot(x, A_copy)
             [y, ycnt, fmax] = sddsolve(s, n)
             
             # Check Progress
             d = np.sqrt(fmax * ycnt) / (ycnt * xcnt)
             beta = d**2 * ycnt * xcnt #not use
             if l > 0: # python is zero-based
-                 if np.dot((x-xold).T, (x-xold))==0 and np.dot((y-yold).T,(y-yold))==0 and dold==d:
+                 if dold==d and np.dot((x-xold), (x-xold))==0 and np.dot((y-yold),(y-yold))==0:
                      break
 
             xold = x
@@ -92,14 +57,22 @@ def sdd(A, kmax=100, alphamin=0.001, lmax=1000, rhomin=0, yinit=2, max_epoch=20,
             #end l-loop
 
         # Save        
-        xsav[:, k] = x.T            # shape conflict (matlab deals with this internally)        
-        ysav[:, k] = y.T
+        xsav[:, k] = x            # shape conflict (matlab deals with this internally)        
+        ysav[:, k] = y
         dsav[k] = d
+
+        A_copy = A_copy - np.dot(xsav[:,k:k+1] * dsav[k], ysav[:,k:k+1].T)  ## about 5s
+        # A_copy = A_copy - (xsav[:,k:k+1] * dsav[k] * ysav[:,k])  ## about 5s
+        # A_copy = A_copy - (x[:, np.newaxis] * d * y)  ## about 5s
+
+    end_time = time.time()
+    print('sdd init ' + str(k+1) + ' of ' + str(kmax) + ' in', end_time-start_time, 's')
+
     #end k-loop
 
     print('sdd init done!')
     if savedir is not None:
-        with open(savedir+'_epoch'+str(1) + '.pkl', 'wb') as f:
+        with open(savedir+'/'+prefix+'sdd_epoch'+str(1) + '.pkl', 'wb') as f:
             pickle.dump({'D': dsav, 'U': xsav, 'V': ysav}, f, pickle.HIGHEST_PROTOCOL)
 
     res_new = math.pow(np.linalg.norm(A-np.dot(np.dot(xsav,np.diag(dsav.flatten().tolist())),ysav.T),'fro'),2)
@@ -109,20 +82,19 @@ def sdd(A, kmax=100, alphamin=0.001, lmax=1000, rhomin=0, yinit=2, max_epoch=20,
     print(res_new)
     for epoch in range(max_epoch-1):
         print('sdd update epoch ' + str(epoch+2) + ' of ' + str(max_epoch))
+        A_copy = np.copy(A)
         for k in range(kmax):
             y = ysav[:, k]
+            dsav[k] = 0
+            A_copy = A - np.dot(np.dot(xsav,np.diag(dsav.flatten().tolist())),ysav.T)
             for l in range (lmax):
                 # Fix y and Solve for x
-                s = np.dot(A,y)            
-                s = s - np.dot(xsav, np.multiply(dsav, np.dot(ysav.T,y)))
-                s = s + np.dot(xsav[:,k], np.multiply(dsav[k], np.dot(ysav[:,k].T, y)))
+                s = np.dot(A_copy,y)            
                 [x, xcnt, _] = sddsolve(s, m)
   
                 # Fix x and Solve for y
                 #s = np.dot(A.T,x)
-                s = np.dot(x, A)
-                s = s - (np.dot(ysav,(np.multiply(dsav,(np.dot(xsav.T,x))))))
-                s = s + np.dot(ysav[:,k], np.multiply(dsav[k], np.dot(xsav[:,k].T, x)))
+                s = np.dot(x, A_copy)
                 [y, ycnt, fmax] = sddsolve(s, n)
                 
                 # Check Progress
@@ -130,7 +102,7 @@ def sdd(A, kmax=100, alphamin=0.001, lmax=1000, rhomin=0, yinit=2, max_epoch=20,
                 beta = d**2 * ycnt * xcnt #not use  
               
                 if l > 0: # python is zero-based
-                     if np.dot((x-xold).T, (x-xold))==0 and np.dot((y-yold).T,(y-yold))==0 and dold==d:
+                     if dold==d and np.dot((x-xold).T, (x-xold))==0 and np.dot((y-yold).T,(y-yold))==0:
                          break
   
                 xold = x
@@ -146,7 +118,7 @@ def sdd(A, kmax=100, alphamin=0.001, lmax=1000, rhomin=0, yinit=2, max_epoch=20,
         res_new = math.pow(np.linalg.norm(A-np.dot(np.dot(xsav,np.diag(dsav.flatten().tolist())),ysav.T),'fro'),2)
         print(res_new)
         if savedir is not None:
-            with open(savedir+'_epoch'+str(epoch+2) + '.pkl', 'wb') as f:
+            with open(savedir+'/'+prefix+'sdd_epoch'+str(epoch+2) + '.pkl', 'wb') as f:
                 pickle.dump({'D': dsav, 'U': xsav, 'V': ysav}, f, pickle.HIGHEST_PROTOCOL)
 
         #Threshold Test
